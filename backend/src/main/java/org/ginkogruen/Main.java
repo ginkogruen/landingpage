89484f6d3d6563ec0c13287bf38db874d4e37011
ginkogruen <jasper@wolter-koeln.de>
2024-10-11 22:29:15 +0200

    Renamed directories

---
+++ backend/src/main/java/org/ginkogruen/Main.java
@@ -0,0 +1,7 @@
+package org.ginkogruen;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Backend online!");
+    }
+}
--- lp-backend/src/main/java/org/ginkogruen/Main.java
+++
@@ -1,5 +0,0 @@
-package org.ginkogruen;
-
-public class Main {
-    public static void main(String[] args) {}
-}
